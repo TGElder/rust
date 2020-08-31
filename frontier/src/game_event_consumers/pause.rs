4e79931b7dfe6ee0c73c231f77422d891786e30e
TGElder <tgelder@gmail.com>
2020-08-31 18:36:46 +0100

    feat: pause

---
+++ frontier/src/game_event_consumers/pause.rs
@@ -0,0 +1,76 @@
+use super::*;
+use crate::simulation::Simulation;
+use commons::futures::executor::block_on;
+use isometric::{Button, ElementState, ModifiersState, VirtualKeyCode};
+
+const HANDLE: &str = "pause";
+
+pub struct Pause {
+    game_tx: UpdateSender<Game>,
+    sim_tx: UpdateSender<Simulation>,
+    binding: Button,
+    pause: bool,
+}
+
+impl Pause {
+    pub fn new(game_tx: &UpdateSender<Game>, sim_tx: &UpdateSender<Simulation>) -> Pause {
+        Pause {
+            game_tx: game_tx.clone_with_handle(HANDLE),
+            sim_tx: sim_tx.clone_with_handle(HANDLE),
+            binding: Button::Key(VirtualKeyCode::Space),
+            pause: true,
+        }
+    }
+
+    fn pause(&mut self) {
+        let game_tx = self.game_tx.clone();
+        self.sim_tx.update(move |sim| {
+            block_on(async {
+                sim.stop_processing_instructions();
+                game_tx.update(|game| game.mut_state().speed = 0.0).await;
+            })
+        });
+    }
+
+    fn resume(&mut self, default_speed: f32) {
+        let game_tx = self.game_tx.clone();
+        self.sim_tx.update(move |sim| {
+            block_on(async {
+                sim.start_processing_instructions();
+                game_tx
+                    .update(move |game| game.mut_state().speed = default_speed)
+                    .await;
+            })
+        });
+    }
+}
+
+impl GameEventConsumer for Pause {
+    fn name(&self) -> &'static str {
+        HANDLE
+    }
+
+    fn consume_game_event(&mut self, _: &GameState, _: &GameEvent) -> CaptureEvent {
+        CaptureEvent::No
+    }
+
+    fn consume_engine_event(&mut self, game_state: &GameState, event: Arc<Event>) -> CaptureEvent {
+        if let Event::Button {
+            ref button,
+            state: ElementState::Pressed,
+            modifiers: ModifiersState { alt: false, .. },
+            ..
+        } = *event
+        {
+            if button == &self.binding {
+                if self.pause {
+                    self.pause();
+                } else {
+                    self.resume(game_state.params.default_speed);
+                }
+                self.pause = !self.pause;
+            }
+        }
+        CaptureEvent::No
+    }
+}
--- frontier/src/game_event_consumers/speed_control.rs
+++ frontier/src/game_event_consumers/speed_control.rs
@@ -2,7 +2,6 @@ use super::*;
 use isometric::{Button, ElementState, VirtualKeyCode};
 
 const HANDLE: &str = "speed_control";
-const SECONDS_PER_HOUR: f32 = 3600.0;
 
 pub struct SpeedControlBindings {
     slow_down: Button,
@@ -20,8 +19,6 @@ impl Default for SpeedControlBindings {
 
 pub struct SpeedControl {
     game_tx: UpdateSender<Game>,
-    hours_per_second: [f32; 13],
-    index: usize,
     bindings: SpeedControlBindings,
 }
 
@@ -29,44 +26,20 @@ impl SpeedControl {
     pub fn new(game_tx: &UpdateSender<Game>) -> SpeedControl {
         SpeedControl {
             game_tx: game_tx.clone_with_handle(HANDLE),
-            hours_per_second: [
-                0.0,
-                0.000_277_778, // Real time
-                0.0625,
-                0.125,
-                0.25,
-                0.5,
-                1.0,
-                2.0,
-                4.0,
-                8.0,
-                16.0,
-                32.0,
-                64.0,
-            ],
-            index: 6,
             bindings: SpeedControlBindings::default(),
         }
     }
 
     fn slow_down(&mut self) {
-        if self.index > 0 {
-            self.index -= 1;
-            self.update_speed();
-        }
+        self.game_tx.update(move |game: &mut Game| {
+            game.mut_state().speed /= 2.0;
+            println!("speed = {}", game.game_state().speed);
+        });
     }
 
     fn speed_up(&mut self) {
-        if self.index < self.hours_per_second.len() - 1 {
-            self.index += 1;
-            self.update_speed();
-        }
-    }
-
-    fn update_speed(&mut self) {
-        let speed = self.hours_per_second[self.index] * SECONDS_PER_HOUR;
         self.game_tx.update(move |game: &mut Game| {
-            game.mut_state().speed = speed;
+            game.mut_state().speed *= 2.0;
             println!("speed = {}", game.game_state().speed);
         });
     }
@@ -77,10 +50,7 @@ impl GameEventConsumer for SpeedControl {
         HANDLE
     }
 
-    fn consume_game_event(&mut self, _: &GameState, event: &GameEvent) -> CaptureEvent {
-        if let GameEvent::Init = event {
-            self.update_speed();
-        }
+    fn consume_game_event(&mut self, _: &GameState, _: &GameEvent) -> CaptureEvent {
         CaptureEvent::No
     }
 
