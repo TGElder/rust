88aa62613d8a576a6a7275380efeffb25932ae25
TGElder <tgelder@gmail.com>
2020-11-12 19:06:44 +0000

    refactor: visibility actor persistence

--- frontier/src/simulation/processors/instruction_logger.rs
+++ frontier/src/simulation/processors/instruction_logger.rs
@@ -1,10 +1,12 @@
 use super::*;
 
 use crate::settlement::Settlement;
+use commons::log::debug;
 
 pub struct InstructionLogger {}
 
 #[async_trait]
+#[allow(clippy::single_match)]
 impl Processor for InstructionLogger {
     async fn process(&mut self, state: State, instruction: &Instruction) -> State {
         match instruction {
@@ -12,11 +14,7 @@ impl Processor for InstructionLogger {
                 name,
                 current_population,
                 ..
-            }) => println!("{} ({})", name, current_population),
-            Instruction::GetRoutes(Demand { resource, .. }) => println!("  - {}", resource.name()),
-            Instruction::GetRouteChanges { route_set, .. } => {
-                println!("    - found {} routes", route_set.len())
-            }
+            }) => debug!("{} ({})", name, current_population),
             _ => (),
         };
         state
