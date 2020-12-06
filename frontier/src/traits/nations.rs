2eb9a94ad0182df62a66da7057cbd2ec5ef4b230
TGElder <tgelder@gmail.com>
2020-12-06 16:01:38 +0000

    refactor: restoring manual town builder

--- frontier/src/traits/nations.rs
+++ frontier/src/traits/nations.rs
@@ -1,6 +1,8 @@
 use crate::nation::NationDescription;
 use crate::traits::send::SendNations;
 use commons::async_trait::async_trait;
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
 
 #[async_trait]
 pub trait GetNationDescription {
@@ -22,3 +24,59 @@ where
         .await
     }
 }
+
+#[async_trait]
+pub trait NationDescriptions {
+    async fn nation_descriptions(&self) -> Vec<NationDescription>;
+}
+
+#[async_trait]
+impl<T> NationDescriptions for T
+where
+    T: SendNations + Sync,
+{
+    async fn nation_descriptions(&self) -> Vec<NationDescription> {
+        self.send_nations(|nations| {
+            nations
+                .values()
+                .map(|nation| nation.description())
+                .cloned()
+                .collect()
+        })
+        .await
+    }
+}
+
+#[async_trait]
+pub trait RandomTownName {
+    async fn random_town_name(&self, nation: String) -> Result<String, NationNotFound>;
+}
+
+#[async_trait]
+impl<T> RandomTownName for T
+where
+    T: SendNations + Sync,
+{
+    async fn random_town_name(&self, nation: String) -> Result<String, NationNotFound> {
+        self.send_nations(|nations| {
+            nations
+                .get_mut(&nation)
+                .map(|nation| nation.get_town_name())
+                .ok_or_else(|| NationNotFound { nation })
+        })
+        .await
+    }
+}
+
+#[derive(Debug)]
+pub struct NationNotFound {
+    nation: String,
+}
+
+impl Display for NationNotFound {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Unknown nation: {}", self.nation)
+    }
+}
+
+impl Error for NationNotFound {}
