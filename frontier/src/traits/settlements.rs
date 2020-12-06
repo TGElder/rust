fb028a79f9d86008a20a75004533ce8bfa4a93ba
Thomas Elder <tgelder@gmail.com>
2020-12-06 14:42:04 +0000

    refactor: remove town

--- frontier/src/traits/settlements.rs
+++ frontier/src/traits/settlements.rs
@@ -1,6 +1,8 @@
 use crate::settlement::{Settlement, SettlementClass};
 use crate::traits::send::SendSettlements;
-use crate::traits::{AddController, DrawTown, RemoveWorldObject};
+use crate::traits::{
+    AddController, DrawTown, Micros, RemoveController, RemoveWorldObject, SetControlDurations,
+};
 use commons::async_trait::async_trait;
 use commons::V2;
 
@@ -64,6 +66,32 @@ where
     async fn get_settlement(&self, position: V2<usize>) -> Option<Settlement> {
         self.send_settlements(move |settlements| settlements.get(&position).cloned())
             .await
+    }
+}
+
+#[async_trait]
+pub trait RemoveTown {
+    async fn remove_town(&self, position: V2<usize>) -> bool;
+}
+
+#[async_trait]
+impl<T> RemoveTown for T
+where
+    T: DrawTown + Micros + RemoveController + SetControlDurations + SendSettlements + Sync,
+{
+    async fn remove_town(&self, position: V2<usize>) -> bool {
+        let settlement = self
+            .send_settlements(move |settlements| settlements.remove(&position))
+            .await;
+        let micros = self.micros().await;
+        let settlement = unwrap_or!(settlement, return false);
+        if let SettlementClass::Town = settlement.class {
+            self.set_control_durations(settlement.position, hashmap! {}, micros)
+                .await;
+            self.remove_controller(settlement.position).await;
+        }
+        self.draw_town(settlement);
+        true
     }
 }
 
