2777fd645bbc8c94fdc2805e8d6a61c1da9bc10b
TGElder <tgelder@gmail.com>
2020-12-06 10:07:59 +0000

    refactor: update_territory trait

---
+++ frontier/src/traits/pathfinder.rs
@@ -0,0 +1,31 @@
+use commons::async_trait::async_trait;
+use std::collections::HashMap;
+use std::time::Duration;
+
+use commons::V2;
+
+use crate::traits::SendPathfinder;
+
+#[async_trait]
+pub trait PositionsWithin {
+    async fn positions_within(
+        &self,
+        positions: Vec<V2<usize>>,
+        duration: Duration,
+    ) -> HashMap<V2<usize>, Duration>;
+}
+
+#[async_trait]
+impl<T> PositionsWithin for T
+where
+    T: SendPathfinder + Sync,
+{
+    async fn positions_within(
+        &self,
+        positions: Vec<V2<usize>>,
+        duration: Duration,
+    ) -> HashMap<V2<usize>, Duration> {
+        self.send_pathfinder(move |pathfinder| pathfinder.positions_within(&positions, &duration))
+            .await
+    }
+}
