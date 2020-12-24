c1643481fc9a02759ea199905a11c4401912ff87
TGElder <tgelder@gmail.com>
2020-12-24 15:17:52 +0000

    refactor: testing and tidying remove road

--- frontier/src/configuration/configuration.rs
+++ frontier/src/configuration/configuration.rs
@@ -21,9 +21,10 @@ use crate::simulation::builders::{CropsBuilder, RoadBuilder, TownBuilder};
 use crate::simulation::demand_fn::{homeland_demand_fn, town_demand_fn};
 use crate::simulation::processors::{
     max_abs_population_change, BuildRoad, BuildSim, BuildTown, GetDemand, GetRouteChanges,
-    GetRoutes, GetTerritory, GetTownTraffic, InstructionLogger, RemoveTown, StepHomeland, StepTown,
-    TryBuildCrops, TryRemoveCrops, TryRemoveRoad, UpdateCurrentPopulation, UpdateEdgeTraffic,
-    UpdateHomelandPopulation, UpdatePositionTraffic, UpdateRouteToPorts, UpdateTown,
+    GetRoutes, GetTerritory, GetTownTraffic, InstructionLogger, RemoveRoad, RemoveTown,
+    StepHomeland, StepTown, TryBuildCrops, TryRemoveCrops, UpdateCurrentPopulation,
+    UpdateEdgeTraffic, UpdateHomelandPopulation, UpdatePositionTraffic, UpdateRouteToPorts,
+    UpdateTown,
 };
 use crate::simulation::Simulation;
 use crate::system::SystemListener;
@@ -139,19 +140,19 @@ impl Configuration {
                         Box::new(GetRouteChanges::new(game_tx)),
                         Box::new(UpdatePositionTraffic::new()),
                         Box::new(UpdateEdgeTraffic::new()),
-                        Box::new(BuildTown::new(x.clone_with_name("try_build_town"))),
+                        Box::new(BuildTown::new(x.clone_with_name("build_town"))),
                         Box::new(TryBuildCrops::new(
                             x.clone_with_name("try_build_crops"),
                             game_state.params.seed,
                         )),
                         Box::new(TryRemoveCrops::new(x.clone_with_name("try_remove_crops"))),
                         Box::new(BuildRoad::new(
-                            x.clone_with_name("try_build_road"),
+                            x.clone_with_name("build_road"),
                             Arc::new(AutoRoadTravelDuration::from_params(
                                 &game_state.params.auto_road_travel,
                             )),
                         )),
-                        Box::new(TryRemoveRoad::new(x.clone_with_name("try_remove_road"))),
+                        Box::new(RemoveRoad::new(x.clone_with_name("remove_road"))),
                         Box::new(UpdateRouteToPorts::new(game_tx)),
                     ],
                 ),
