35f8161ceeacfb2a2fe6f8e4d31fc09038201ad2
Thomas Elder <tgelder@gmail.com>
2020-12-26 09:58:12 +0000

    refactor: removing some unused game traits

--- frontier/src/game/traits/mod.rs
+++ frontier/src/game/traits/mod.rs
@@ -2,18 +2,14 @@ mod check_for_port;
 mod controlled;
 mod get_route;
 mod micros;
-mod nations;
 mod routes;
 mod settlements;
-mod who_controls_tile;
 mod world;
 
 pub use check_for_port::*;
 pub use controlled::*;
 pub use get_route::*;
 pub use micros::*;
-pub use nations::*;
 pub use routes::*;
 pub use settlements::*;
-pub use who_controls_tile::*;
 pub use world::*;
