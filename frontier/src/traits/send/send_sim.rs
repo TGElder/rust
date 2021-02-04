3c6d1820f9fba9206a5c8589b10d8cb08ab3f4b3
Thomas Elder <tgelder@gmail.com>
2021-02-04 20:35:00 +0000

    refactor: removing traffic from sim state

--- frontier/src/traits/send/send_sim.rs
+++ frontier/src/traits/send/send_sim.rs
@@ -8,10 +8,10 @@ pub trait SendSim: SendWorld + Send {
     async fn send_sim<F, O>(&self, function: F) -> O
     where
         O: Send + 'static,
-        F: FnOnce(&mut Simulation<Self>) -> O + Send + 'static;
+        F: FnOnce(&mut Simulation) -> O + Send + 'static;
 
     fn send_sim_background<F, O>(&self, function: F)
     where
         O: Send + 'static,
-        F: FnOnce(&mut Simulation<Self>) -> O + Send + 'static;
+        F: FnOnce(&mut Simulation) -> O + Send + 'static;
 }
