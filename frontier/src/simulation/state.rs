044d69fefaa59620c525562e2f7526b695fe4df0
Thomas Elder <tgelder@gmail.com>
2021-02-06 10:34:26 +0000

    refactor: removing parameters from sim state

--- frontier/src/simulation/state.rs
+++ frontier/src/simulation/state.rs
@@ -4,14 +4,12 @@ use std::default::Default;
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct State {
-    pub params: SimulationParams,
     pub instructions: Vec<Instruction>,
 }
 
 impl Default for State {
     fn default() -> State {
         State {
-            params: SimulationParams::default(),
             instructions: vec![],
         }
     }
