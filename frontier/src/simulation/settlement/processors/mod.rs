60bc9f37cddee9b1c88b72efb39798f74ec3fca0
Thomas Elder <tgelder@gmail.com>
2021-02-13 09:28:29 +0000

    refactor: tidying up get route changes extension

--- frontier/src/simulation/settlement/processors/mod.rs
+++ frontier/src/simulation/settlement/processors/mod.rs
@@ -1,13 +1,11 @@
 use super::*;
 
-mod get_route_changes;
 mod step_homeland;
 mod step_town;
 mod update_edge_traffic;
 mod update_position_traffic;
 mod update_route_to_ports;
 
-pub use get_route_changes::*;
 pub use step_homeland::*;
 pub use step_town::*;
 pub use update_edge_traffic::*;
