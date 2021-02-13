27b7e885ccf0b2715495e4909c499ae311fb33f7
Thomas Elder <tgelder@gmail.com>
2021-02-13 09:35:04 +0000

    refactor: update edge traffic extension

--- frontier/src/simulation/settlement/extensions/mod.rs
+++ frontier/src/simulation/settlement/extensions/mod.rs
@@ -4,6 +4,7 @@ mod get_territory;
 mod get_town_traffic;
 mod remove_town;
 mod update_current_population;
+mod update_edge_traffic;
 mod update_homeland;
 mod update_town;
 
@@ -13,5 +14,6 @@ pub use get_territory::*;
 pub use get_town_traffic::*;
 pub use remove_town::*;
 pub use update_current_population::*;
+pub use update_edge_traffic::*;
 pub use update_homeland::*;
 pub use update_town::*;
