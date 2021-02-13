79c1cf3b4a54b701af86af96bb04dd3bc93f1844
Thomas Elder <tgelder@gmail.com>
2021-02-13 15:45:43 +0000

    feat: set sim threads from command line

--- frontier/src/args.rs
+++ frontier/src/args.rs
@@ -2,10 +2,12 @@ pub enum Args {
     New {
         power: usize,
         seed: u64,
+        threads: usize,
         reveal_all: bool,
     },
     Load {
         path: String,
+        threads: usize,
     },
 }
 
@@ -16,11 +18,13 @@ impl Args {
             Args::New {
                 power: args[1].parse().unwrap(),
                 seed: args[2].parse().unwrap(),
+                threads: args[3].parse().unwrap(),
                 reveal_all: args.contains(&"-r".to_string()),
             }
         } else if args.len() == 2 {
             Args::Load {
                 path: args[1].clone(),
+                threads: args[2].parse().unwrap(),
             }
         } else {
             panic!("Invalid command line arguments");
