5d527496f3a1a451e9c42241abbda8b61674c280
Thomas Elder <tgelder@gmail.com>
2021-02-13 15:53:15 +0000

    fix: thread arg not working for loading

--- frontier/src/args.rs
+++ frontier/src/args.rs
@@ -14,14 +14,14 @@ pub enum Args {
 #[allow(clippy::comparison_chain)]
 impl Args {
     pub fn new(args: Vec<String>) -> Args {
-        if args.len() > 2 {
+        if args.len() > 3 {
             Args::New {
                 power: args[1].parse().unwrap(),
                 seed: args[2].parse().unwrap(),
                 threads: args[3].parse().unwrap(),
                 reveal_all: args.contains(&"-r".to_string()),
             }
-        } else if args.len() == 2 {
+        } else if args.len() == 3 {
             Args::Load {
                 path: args[1].clone(),
                 threads: args[2].parse().unwrap(),
