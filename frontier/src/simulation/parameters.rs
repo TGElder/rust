6cc080ceec060a645383acc7303597490cfe67d9
Thomas Elder <tgelder@gmail.com>
2021-02-14 14:56:54 +0000

    chore: updating dependency graph

--- frontier/src/simulation/parameters.rs
+++ frontier/src/simulation/parameters.rs
@@ -5,24 +5,24 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Copy, Debug, PartialEq, Deserialize, Serialize)]
 pub struct SimulationParameters {
     pub initial_town_population: f64,
+    pub max_abs_population_change: MaxAbsPopulationChange,
     pub nation_flip_traffic_pc: f64,
     pub road_build_threshold: usize,
+    pub threads: usize,
     pub town_removal_population: f64,
     pub traffic_to_population: f64,
-    pub max_abs_population_change: MaxAbsPopulationChange,
-    pub threads: usize,
 }
 
 impl Default for SimulationParameters {
     fn default() -> Self {
         SimulationParameters {
             initial_town_population: 0.5,
+            max_abs_population_change: MaxAbsPopulationChange::default(),
             nation_flip_traffic_pc: 0.67,
             road_build_threshold: 8,
+            threads: 2,
             town_removal_population: 0.25,
             traffic_to_population: 0.5,
-            max_abs_population_change: MaxAbsPopulationChange::default(),
-            threads: 2,
         }
     }
 }
