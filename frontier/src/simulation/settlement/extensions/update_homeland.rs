bf42139ba7a562bdbeeabae2037ee5bc09df8d7f
Thomas Elder <tgelder@gmail.com>
2021-02-14 17:12:14 +0000

    fix: settlements not updating

--- frontier/src/simulation/settlement/extensions/update_homeland.rs
+++ frontier/src/simulation/settlement/extensions/update_homeland.rs
@@ -1,26 +1,20 @@
 use crate::settlement::Settlement;
 use crate::simulation::settlement::SettlementSimulation;
 use crate::traits::has::HasParameters;
-use crate::traits::{UpdateSettlement as UpdateSettlementTrait, VisibleLandPositions};
+use crate::traits::VisibleLandPositions;
 
 impl<T> SettlementSimulation<T>
 where
-    T: HasParameters + UpdateSettlementTrait + VisibleLandPositions,
+    T: HasParameters + VisibleLandPositions,
 {
-    pub async fn update_homeland(&self, settlement: &Settlement) {
+    pub async fn update_homeland(&self, settlement: Settlement) -> Settlement {
         let visible_land_positions = self.tx.visible_land_positions().await;
         let homeland_count = self.tx.parameters().homeland.count;
         let target_population = visible_land_positions as f64 / homeland_count as f64;
-        self.update_population(settlement.clone(), target_population)
-            .await;
-    }
-
-    async fn update_population(&self, settlement: Settlement, target_population: f64) {
-        let settlement = Settlement {
+        Settlement {
             target_population,
             ..settlement
-        };
-        self.tx.update_settlement(settlement).await;
+        }
     }
 }
 
@@ -30,14 +24,11 @@ mod tests {
     use crate::parameters::Parameters;
     use crate::settlement::SettlementClass::Homeland;
     use commons::async_trait::async_trait;
-    use commons::{v2, V2};
+    use commons::v2;
     use futures::executor::block_on;
-    use std::collections::HashMap;
-    use std::sync::Mutex;
 
     struct Tx {
         parameters: Parameters,
-        settlements: Mutex<HashMap<V2<usize>, Settlement>>,
         visible_land_positions: usize,
     }
 
@@ -54,49 +45,33 @@ mod tests {
         }
     }
 
-    #[async_trait]
-    impl UpdateSettlementTrait for Tx {
-        async fn update_settlement(&self, settlement: Settlement) {
-            self.settlements
-                .lock()
-                .unwrap()
-                .insert(settlement.position, settlement);
-        }
-    }
-
     #[test]
     fn target_population_should_be_equal_share_of_visible_land() {
         // Given
-        let settlement = Settlement {
-            position: v2(0, 1),
-            class: Homeland,
-            ..Settlement::default()
-        };
-        let settlements = Mutex::new(hashmap! {
-            v2(0, 1) => settlement.clone()
-        });
-
         let mut tx = Tx {
             parameters: Parameters::default(),
-            settlements,
             visible_land_positions: 202,
         };
         tx.parameters.homeland.count = 2;
         let sim = SettlementSimulation::new(tx);
 
         // When
-        block_on(sim.update_homeland(&settlement));
+        let settlement = Settlement {
+            position: v2(0, 1),
+            class: Homeland,
+            ..Settlement::default()
+        };
+        let updated = block_on(sim.update_homeland(settlement));
 
         // Then
-        let actual = sim.tx.settlements.lock().unwrap();
-        let expected = hashmap! {
-            v2(0, 1) => Settlement{
+        assert_eq!(
+            updated,
+            Settlement {
                 position: v2(0, 1),
                 class: Homeland,
                 target_population: 101.0,
                 ..Settlement::default()
             }
-        };
-        assert_eq!(*actual, expected);
+        );
     }
 }
