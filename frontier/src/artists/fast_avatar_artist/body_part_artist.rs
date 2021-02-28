8f79b63cbe025aa271ae11d33dacee5453ee0b33
Thomas Elder <tgelder@gmail.com>
2021-02-28 19:27:51 +0000

    more sensible params

--- frontier/src/artists/fast_avatar_artist/body_part_artist.rs
+++ frontier/src/artists/fast_avatar_artist/body_part_artist.rs
@@ -8,10 +8,10 @@ use isometric::drawing::{
     update_billboards_vertices, update_masked_billboard_mask, update_masked_billboard_texture,
     update_masked_billboards_vertices,
 };
-use isometric::{Color, Command};
+use isometric::Command;
 
 use super::artist_avatar::ArtistAvatar;
-use crate::avatar::Avatar;
+use super::parameters::BodyPart;
 
 pub struct BodyPartArtist {
     body_part: BodyPart,
@@ -44,27 +44,27 @@ impl BodyPartArtist {
         if let Some(mask) = &self.body_part.mask {
             Box::new(
                 once(create_masked_billboards(
-                    self.body_part.handle.clone(),
+                    self.body_part.handle.to_string(),
                     max_avatars,
                 ))
                 .chain(once(update_masked_billboard_texture(
-                    self.body_part.handle.clone(),
-                    &self.body_part.texture,
+                    self.body_part.handle.to_string(),
+                    self.body_part.texture,
                 )))
                 .chain(once(update_masked_billboard_mask(
-                    self.body_part.handle.clone(),
-                    &mask.mask,
+                    self.body_part.handle.to_string(),
+                    mask.mask,
                 ))),
             )
         } else {
             Box::new(
                 once(create_billboards(
-                    self.body_part.handle.clone(),
+                    self.body_part.handle.to_string(),
                     max_avatars,
                 ))
                 .chain(once(update_billboard_texture(
-                    self.body_part.handle.clone(),
-                    &self.body_part.texture,
+                    self.body_part.handle.to_string(),
+                    self.body_part.texture,
                 ))),
             )
         }
@@ -89,10 +89,10 @@ impl BodyPartArtist {
         if let Some(mask) = &self.body_part.mask {
             let colors = avatars
                 .iter()
-                .map(|ArtistAvatar { avatar, .. }| mask.color.get(avatar))
+                .map(|ArtistAvatar { avatar, .. }| (mask.color_fn)(avatar))
                 .collect::<Vec<_>>();
             update_masked_billboards_vertices(
-                self.body_part.handle.clone(),
+                self.body_part.handle.to_string(),
                 world_coords,
                 colors,
                 self.width,
@@ -100,41 +100,11 @@ impl BodyPartArtist {
             )
         } else {
             update_billboards_vertices(
-                self.body_part.handle.clone(),
+                self.body_part.handle.to_string(),
                 world_coords,
                 self.width,
                 self.height,
             )
-        }
-    }
-}
-#[derive(Clone)]
-pub struct BodyPart {
-    pub offset: V3<f32>,
-    pub handle: String,
-    pub texture: String,
-    pub texture_width: usize,
-    pub texture_height: usize,
-    pub mask: Option<ColorMask>,
-}
-
-#[derive(Clone)]
-pub struct ColorMask {
-    pub color: AvatarColor,
-    pub mask: String,
-}
-
-#[derive(Clone)]
-pub enum AvatarColor {
-    Base,
-    Skin,
-}
-
-impl AvatarColor {
-    fn get<'a>(&'a self, avatar: &'a Avatar) -> &'a Color {
-        match self {
-            AvatarColor::Base => &avatar.color,
-            AvatarColor::Skin => &avatar.skin_color,
         }
     }
 }
--- frontier/src/artists/fast_avatar_artist/parameters.rs
+++ frontier/src/artists/fast_avatar_artist/parameters.rs
@@ -1,6 +1,7 @@
-use commons::v3;
+use commons::{v3, V3};
+use isometric::Color;
 
-use super::body_part_artist::{AvatarColor, BodyPart, ColorMask};
+use crate::avatar::Avatar;
 
 pub struct AvatarArtistParams {
     pub pixels_per_cell: f32,
@@ -14,65 +15,81 @@ impl AvatarArtistParams {
             body_parts: vec![
                 BodyPart {
                     offset: v3(0.0, 0.0, 96.0),
-                    handle: "body".to_string(),
-                    texture: "resources/textures/body.png".to_string(),
+                    handle: "body",
+                    texture: "resources/textures/body.png",
                     texture_width: 128,
                     texture_height: 192,
                     mask: Some(ColorMask {
-                        mask: "resources/textures/body.png".to_string(),
-                        color: AvatarColor::Base,
+                        mask: "resources/textures/body.png",
+                        color_fn: |avatar| &avatar.color,
                     }),
                 },
                 BodyPart {
                     offset: v3(12.0, 0.0, 192.0),
-                    handle: "head".to_string(),
-                    texture: "resources/textures/head.png".to_string(),
+                    handle: "head",
+                    texture: "resources/textures/head.png",
                     texture_width: 96,
                     texture_height: 96,
                     mask: Some(ColorMask {
-                        mask: "resources/textures/head.png".to_string(),
-                        color: AvatarColor::Skin,
+                        mask: "resources/textures/head.png",
+                        color_fn: |avatar| &avatar.skin_color,
                     }),
                 },
                 BodyPart {
                     offset: v3(48.0, 24.0, 192.0),
-                    handle: "left_eye".to_string(),
-                    texture: "resources/textures/eye.png".to_string(),
+                    handle: "left_eye",
+                    texture: "resources/textures/eye.png",
                     texture_width: 16,
                     texture_height: 16,
                     mask: None,
                 },
                 BodyPart {
                     offset: v3(48.0, -24.0, 192.0),
-                    handle: "right_eye".to_string(),
-                    texture: "resources/textures/eye.png".to_string(),
+                    handle: "right_eye",
+                    texture: "resources/textures/eye.png",
                     texture_width: 16,
                     texture_height: 16,
                     mask: None,
                 },
                 BodyPart {
                     offset: v3(48.0, 50.0, 96.0),
-                    handle: "left_hand".to_string(),
-                    texture: "resources/textures/hand.png".to_string(),
+                    handle: "left_hand",
+                    texture: "resources/textures/hand.png",
                     texture_width: 32,
                     texture_height: 32,
                     mask: Some(ColorMask {
-                        mask: "resources/textures/hand.png".to_string(),
-                        color: AvatarColor::Skin,
+                        mask: "resources/textures/hand.png",
+                        color_fn: |avatar| &avatar.skin_color,
                     }),
                 },
                 BodyPart {
                     offset: v3(48.0, -50.0, 96.0),
-                    handle: "right_hand".to_string(),
-                    texture: "resources/textures/hand.png".to_string(),
+                    handle: "right_hand",
+                    texture: "resources/textures/hand.png",
                     texture_width: 32,
                     texture_height: 32,
                     mask: Some(ColorMask {
-                        mask: "resources/textures/hand.png".to_string(),
-                        color: AvatarColor::Skin,
+                        mask: "resources/textures/hand.png",
+                        color_fn: |avatar| &avatar.skin_color,
                     }),
                 },
             ],
         }
     }
+}
+
+#[derive(Clone)]
+pub struct BodyPart {
+    pub offset: V3<f32>,
+    pub handle: &'static str,
+    pub texture: &'static str,
+    pub texture_width: usize,
+    pub texture_height: usize,
+    pub mask: Option<ColorMask>,
+}
+
+#[derive(Clone)]
+pub struct ColorMask {
+    pub mask: &'static str,
+    pub color_fn: fn(&Avatar) -> &Color,
 }
