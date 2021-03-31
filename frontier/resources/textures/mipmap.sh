6b702ea9749b1966af0103531a2061f987ff5747
Thomas Elder <tgelder@gmail.com>
2021-03-31 20:21:52 +0100

    review changes

--- frontier/resources/textures/mipmap.sh
+++ frontier/resources/textures/mipmap.sh
@@ -1,7 +1,7 @@
 svg=$1
 width=$2
 height=$3
-alias=$4
+antialias=$4
 
 directory="${svg%%.*}"
 rm -rf $directory
@@ -11,10 +11,10 @@ while true; do
 
   out=$directory/${width}x${height}.png
 
-  if [ -z "$alias" ]; then
-    rsvg-convert -w $width -h $height -f png $1 > $out
+  if [ "$antialias" = true ]; then
+    magick -size ${width}x${height} -background none $svg -channel alpha -threshold 33% -channel RGB $out
   else
-    magick -size ${width}x${height} -background none $1 -channel alpha -threshold 33% -channel RGB $out
+    rsvg-convert -w $width -h $height -f png $svg > $out
   fi
 
   echo $out
--- isometric/src/graphics/texture.rs
+++ isometric/src/graphics/texture.rs
@@ -1,15 +1,13 @@
-use commons::image;
-use commons::log::debug;
+use commons::image::{self, ImageError};
 use image::{DynamicImage, GenericImageView};
 use std::collections::HashMap;
 use std::ffi::c_void;
+use std::io;
 use std::path::Path;
 use std::sync::Arc;
 
 pub struct Texture {
     id: gl::types::GLuint,
-    width: u32,
-    height: u32,
 }
 
 impl Texture {
@@ -17,11 +15,7 @@ impl Texture {
         let mut id: gl::types::GLuint = 0;
         unsafe {
             gl::GenTextures(1, &mut id);
-            let mut out = Texture {
-                id,
-                width: 0,
-                height: 0,
-            };
+            let mut out = Texture { id };
             out.load_images(images);
             out
         }
@@ -29,14 +23,6 @@ impl Texture {
 
     pub fn id(&self) -> &gl::types::GLuint {
         &self.id
-    }
-
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
     }
 
     #[allow(clippy::missing_safety_doc)]
@@ -63,11 +49,7 @@ impl Texture {
                 gl::TEXTURE_MIN_FILTER,
                 gl::NEAREST_MIPMAP_NEAREST as i32,
             );
-            gl::TexParameteri(
-                gl::TEXTURE_2D,
-                gl::TEXTURE_MAG_FILTER,
-                gl::NEAREST as i32,
-            );
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
             gl::TexParameteri(
                 gl::TEXTURE_2D,
                 gl::TEXTURE_MAX_LEVEL,
@@ -82,11 +64,8 @@ impl Texture {
 
     unsafe fn load_image(&mut self, image: DynamicImage, level: i32) {
         let dimensions = image.dimensions();
-        self.width = dimensions.0;
-        self.height = dimensions.1;
         let image = image.to_rgba().into_raw();
         let image_ptr: *const c_void = image.as_ptr() as *const c_void;
-        debug!("Loading {}x{} image", dimensions.0, dimensions.1);
         gl::TexImage2D(
             gl::TEXTURE_2D,
             level,
@@ -115,26 +94,63 @@ pub struct TextureLibrary {
 }
 
 impl TextureLibrary {
-    pub fn get_texture(&mut self, file: &str) -> Arc<Texture> {
+    pub fn get_texture(&mut self, path: &str) -> Arc<Texture> {
         self.textures
-            .entry(file.to_string())
-            .or_insert_with(|| Self::load_texture(file))
+            .entry(path.to_string())
+            .or_insert_with(|| {
+                Self::load_texture(path)
+                    .unwrap_or_else(|err| panic!("Could not load texture from {}: {:?}", path, err))
+            })
             .clone()
     }
 
-    fn load_texture(file: &str) -> Arc<Texture> {
-        let path = Path::new(file);
-        if std::path::Path::is_dir(&path) {
-            let images = path
-                .read_dir()
-                .unwrap()
-                .map(|entry| image::open(entry.unwrap().path().to_str().unwrap()).unwrap())
-                .collect();
-            let texture = Texture::new(images);
-            Arc::new(texture)
+    fn load_texture(path: &str) -> Result<Arc<Texture>, TextureLibraryError> {
+        let path = Path::new(path);
+        if path.is_dir() {
+            Self::load_directory(path)
         } else {
-            let texture = Texture::new(vec![image::open(file).unwrap()]);
-            Arc::new(texture)
+            Self::load_file(path)
         }
+    }
+
+    fn load_directory(directory: &Path) -> Result<Arc<Texture>, TextureLibraryError> {
+        let mut images = vec![];
+        for file in directory.read_dir()? {
+            images.push(image::open(
+                file?.path().to_str().ok_or("Non-unicode path")?,
+            )?);
+        }
+        let texture = Texture::new(images);
+        Ok(Arc::new(texture))
+    }
+
+    fn load_file(path: &Path) -> Result<Arc<Texture>, TextureLibraryError> {
+        let texture = Texture::new(vec![image::open(path)?]);
+        Ok(Arc::new(texture))
+    }
+}
+
+#[derive(Debug)]
+pub enum TextureLibraryError {
+    IO(io::Error),
+    Str(&'static str),
+    Image(ImageError),
+}
+
+impl From<io::Error> for TextureLibraryError {
+    fn from(error: io::Error) -> Self {
+        Self::IO(error)
+    }
+}
+
+impl From<&'static str> for TextureLibraryError {
+    fn from(error: &'static str) -> Self {
+        Self::Str(error)
+    }
+}
+
+impl From<ImageError> for TextureLibraryError {
+    fn from(error: ImageError) -> Self {
+        Self::Image(error)
     }
 }
