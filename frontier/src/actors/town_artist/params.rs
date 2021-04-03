405311572e80916c61941ad2c198015b3b0b2a0a
Thomas Elder <tgelder@gmail.com>
2021-04-03 10:44:19 +0100

    feat: fixed house heights

--- frontier/src/actors/town_artist/params.rs
+++ frontier/src/actors/town_artist/params.rs
@@ -5,6 +5,7 @@ use std::default::Default;
 #[derive(Clone, Copy, Debug, PartialEq, Deserialize, Serialize)]
 pub struct TownArtistParameters {
     pub house_width: f32,
+    pub house_height: f32,
     pub house_roof_height: f32,
     pub house_height_log_base: f64,
     pub light_direction: V3<f32>,
@@ -15,6 +16,7 @@ impl Default for TownArtistParameters {
     fn default() -> TownArtistParameters {
         TownArtistParameters {
             house_width: 0.25,
+            house_height: 0.25,
             house_roof_height: 0.5,
             house_height_log_base: 10.0,
             light_direction: v3(0.0, 8.0, -1.0),
