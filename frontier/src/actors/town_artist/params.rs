5f90e8e1d6d920e304920204eeb637e6cea98d1e
Thomas Elder <tgelder@gmail.com>
2021-04-29 18:51:37 +0100

    tweak: lowering height of town house

--- frontier/src/actors/town_artist/params.rs
+++ frontier/src/actors/town_artist/params.rs
@@ -16,7 +16,7 @@ impl Default for TownArtistParameters {
         TownArtistParameters {
             house_width: 0.25,
             house_height: 0.25,
-            house_roof_height: 0.5,
+            house_roof_height: 0.25,
             light_direction: v3(0.0, 8.0, -1.0),
             label_float: 0.33,
         }
