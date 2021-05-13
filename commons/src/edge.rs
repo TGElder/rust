e419f0f792153e84049cd122d5f3ee535301cf1c
Thomas Elder <tgelder@gmail.com>
2021-05-13 20:27:25 +0100

    feat: unit edge methods

--- commons/src/edge.rs
+++ commons/src/edge.rs
@@ -1,4 +1,4 @@
-use crate::V2;
+use crate::{v2, V2};
 use serde::{Deserialize, Serialize};
 
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
@@ -30,10 +30,28 @@ impl Edge {
     pub fn horizontal(&self) -> bool {
         self.from.y == self.to.y
     }
+
+    pub fn unit_edges(&self) -> impl Iterator<Item = Edge> {
+        let from_x = self.from.x;
+        let to_x = self.to.x;
+        let from_y = self.from.y;
+        let to_y = self.to.y;
+
+        let from = (from_x..=to_x).flat_map(move |x| (from_y..=to_y).map(move |y| v2(x, y)));
+        let to = (from_x..=to_x)
+            .flat_map(move |x| (from_y..=to_y).map(move |y| v2(x, y)))
+            .skip(1);
+
+        from.zip(to).map(|(from, to)| Edge::new(from, to))
+    }
 }
 
 pub trait Edges {
     fn edges<'a>(&'a self) -> Box<dyn Iterator<Item = Edge> + 'a>;
+
+    fn unit_edges<'a>(&'a self) -> Box<dyn Iterator<Item = Edge> + 'a> {
+        Box::new(self.edges().flat_map(|edge| edge.unit_edges()))
+    }
 }
 
 impl Edges for [V2<usize>] {
@@ -101,6 +119,24 @@ mod tests {
     }
 
     #[test]
+    fn test_unit_edges_horizontal() {
+        let edge = Edge::new(v2(1, 1), v2(3, 1));
+        assert_eq!(
+            edge.unit_edges().collect::<Vec<_>>(),
+            vec![Edge::new(v2(1, 1), v2(2, 1)), Edge::new(v2(2, 1), v2(3, 1))]
+        );
+    }
+
+    #[test]
+    fn test_unit_edges_vertical() {
+        let edge = Edge::new(v2(1, 1), v2(1, 3));
+        assert_eq!(
+            edge.unit_edges().collect::<Vec<_>>(),
+            vec![Edge::new(v2(1, 1), v2(1, 2)), Edge::new(v2(1, 2), v2(1, 3))]
+        );
+    }
+
+    #[test]
     fn test_edges_with_vector() {
         let positions = vec![v2(0, 0), v2(1, 0), v2(2, 0)];
         let edges: Vec<Edge> = positions.edges().collect();
@@ -132,5 +168,21 @@ mod tests {
         let positions = vec![];
         let edges = positions.edges();
         assert_eq!(edges.count(), 0);
+    }
+
+    #[test]
+    fn test_unit_edges_with_vector() {
+        let positions = vec![v2(0, 0), v2(2, 0), v2(3, 0), v2(3, 2)];
+        let edges: Vec<Edge> = positions.unit_edges().collect();
+        assert_eq!(
+            edges,
+            vec![
+                Edge::new(v2(0, 0), v2(1, 0)),
+                Edge::new(v2(1, 0), v2(2, 0)),
+                Edge::new(v2(2, 0), v2(3, 0)),
+                Edge::new(v2(3, 0), v2(3, 1)),
+                Edge::new(v2(3, 1), v2(3, 2))
+            ]
+        );
     }
 }
