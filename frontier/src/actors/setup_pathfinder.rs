f14271eb836b9c43338ea5a5fb8fb1928723cd3a
Thomas Elder <tgelder@gmail.com>
2021-05-18 19:25:36 +0100

    feat: updating pathfinders with bridges at startup

--- frontier/src/actors/setup_pathfinder.rs
+++ frontier/src/actors/setup_pathfinder.rs
@@ -1,24 +1,25 @@
 use commons::v2;
 
 use crate::traits::has::HasParameters;
-use crate::traits::UpdatePositionsAllPathfinders;
+use crate::traits::{UpdateEdgesAllPathfinders, UpdatePositionsAllPathfinders, WithBridges};
 
-pub struct SetupPathfinders<T>
-where
-    T: HasParameters + UpdatePositionsAllPathfinders,
-{
+pub struct SetupPathfinders<T> {
     cx: T,
 }
 
 impl<T> SetupPathfinders<T>
 where
-    T: HasParameters + UpdatePositionsAllPathfinders,
+    T: HasParameters + UpdateEdgesAllPathfinders + UpdatePositionsAllPathfinders + WithBridges,
 {
     pub fn new(cx: T) -> SetupPathfinders<T> {
         SetupPathfinders { cx }
     }
 
-    pub async fn init(&mut self) {
+    pub async fn init(&self) {
+        join!(self.init_positions(), self.init_bridges());
+    }
+
+    async fn init_positions(&self) {
         let width = self.cx.parameters().width;
 
         let all_positions = (0..width).flat_map(move |x| (0..width).map(move |y| v2(x, y)));
@@ -26,5 +27,16 @@ where
         self.cx
             .update_positions_all_pathfinders(all_positions)
             .await;
+    }
+
+    async fn init_bridges(&self) {
+        let bridges = self.cx.with_bridges(|bridges| (*bridges).clone()).await;
+
+        let edge_durations = bridges
+            .values()
+            .flat_map(|bridge| bridge.edge_durations())
+            .collect::<Vec<_>>();
+
+        self.cx.update_edges_all_pathfinders(edge_durations).await;
     }
 }
